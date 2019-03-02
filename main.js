f151a4f896a111ca7fa333acff8e71d175da7094
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-03-02 11:07:55 -0600

    function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log('hi');
+
+const firstName = 'Lindsey';
+const lastName = 'Maddox';
+
+// console.log(firstName + ' ' + lastName);
+
+console.log(`${firstName} ${lastName}`);  // Do this now cause ES6
+
+const namePrinter = (firstName, lastName) => {
+  console.log(`${firstName} ${lastName}`);
+};
+
+namePrinter('Lindsey', 'Maddox');
+namePrinter('Azalea', 'Ruth');
